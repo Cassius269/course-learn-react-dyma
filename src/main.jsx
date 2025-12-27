9010b14cce6f7bf821e88d73f3555daad86d73be
Cassius269 <fahamygaston@gmail.com>
2025-12-27 19:17:07 +0100

    création d'élements React avec la méthode createElement()

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React, { StrictMode } from 'react';
 
+// Création d'éléments React
+const root = createRoot(document.getElementById('root'));
+
+const title = React.createElement(
+  'h1',
+  { className: 'myClass'},
+  'Hello world'
+);
+
+
+
+const section = React.createElement(
+  'section', 
+  {className: 'sectionPresentation'},
+  title // injecter l'élement React de titre dans la section
+);
+
+root.render(section);  // injecter l'élement React de section dans l'élement racine
+/*
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+*/
