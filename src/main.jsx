8842d28f2ec9202350e51788e47deeabfff6e9a9
Cassius269 <fahamygaston@gmail.com>
2025-12-28 21:21:44 +0100

    notions de JSX

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,37 @@ const root = createRoot(document.querySelector('#root'));
 // Création d'éléments React
 const image = <img src="https://images.pexels.com/photos/31086170/pexels-photo-31086170.jpeg" alt="" width={200} />;
 
+const number = 4;
+
+function add(a,b){
+  return a+b;
+};
+
+const myclass = 'title';
 const component = () => {
-  return <h1 className='title'>Hello world !!</h1>;
+  return <h1 className={myclass}>Hello world !! * {add(1,2)}</h1>;
 }
 
+const Content = (
+  <>
+    <section>
+      <h1>Le gros titre</h1>
+      <p>Premier paragraphe</p>
+    </section>
+    <p>Hello world</p>
+  </>
+);
 
-root.render(image);
+// Convertir du HTML en JSX
+const ComponentB = () => {
+  return <section>
+          <h1>La todolist de Hedi</h1>
+          <ul>
+            <li>Aller au cinéma</li>
+            <li>Faire du sport</li>
+            <li>Aller au restaurant</li>
+          </ul>
+        </section>
+}      ;
+
+root.render(ComponentB());
