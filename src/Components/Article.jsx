fb60a13bdaffbcb39c3d9b32ecee38b321dbcb5a
Cassius269 <fahamygaston@gmail.com>
2025-12-29 19:29:58 +0100

    stylisation de composant

---
+++ src/Components/Article.jsx
@@ -0,0 +1,10 @@
+export default function Article ()  {
+    const color = "white";
+    return (
+            <>
+                <h1 style={{ color : "white", textAlign: "center", backgroundColor: "orange"}}>Titre de l'article</h1>
+                <p>Contenu de l'article</p>
+            </>
+    );
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import { Profile,  Image } from './Components/Profile';
+import Article from './components/article';
+
 
 // Récupération de l'élement racine
 const root = createRoot(document.querySelector('#root'));
 
-// Création de composant React
-function SayHello(){
-  return <h1>Hello world !!</h1>
-}
-
 
 
 root.render(
-  <>
-    <Profile name="Jean" age={12} />  
-    <Image/>
-    <Image/>
-    <Profile name="Pierre" age={10}>
-      <button>Cliquer</button>
-    </Profile>
-  </>
+    <Article/>
 ); 
