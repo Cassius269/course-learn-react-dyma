0957b24cf11275a89471e3a297eeeb0d789a17c9
Cassius269 <fahamygaston@gmail.com>
2026-01-03 22:26:39 +0100

    manipulation de tableau d'objets à l'aide de la high function map()

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,7 @@ export default function App(){
                 <section>
                     <h2 className='mt-5 mb-3'>Liste des articles</h2>
                     <div className='row d-flex justify-content-center gap-4 gap-md-4 gap-lg-5'>
-                        <Article displayArticle={true} />  
-                        <Article displayArticle={true} />  
-                        <Article displayArticle={false}/>  
-                        <Article displayArticle={true} />  
-                        <Article displayArticle={true} />  
-                        <Article displayArticle={true} />  
+                        <Article displayArticle={true} />
                     </div>
                 </section>
             </main>
--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -3,18 +3,37 @@ import '../assets/styles/layouts/Footer.module.scss';
 export default function Article ({displayArticle})  {
 // console.log({displayArticle});
 
-    return  displayArticle && (<article className='mt-4 card col-8 col-md-6 col-lg-3'>
-                                <div className='card-body'>
-                                    <h3 className='bg-primary py-3 text-white card-title'>Lorem ipsum dolor sit amet</h3>
-                                    <p className='card-text text-body-secondary'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita iure recusandae deleniti ullam reiciendis, eaque totam eum itaque at temporibus quos libero quo, deserunt tempora. Natus libero iusto asperiores sunt.
-                                    Tempore maiores repellendus illum ad. Voluptatem autem deserunt asperiores, hic et amet labore facilis totam quibusdam assumenda pariatur aliquam quo quos? Facilis deserunt tenetur eaque saepe nemo quos dolorem distinctio.
-                                    Nobis beatae nostrum repellendus quos ratione. Tempore quam quo totam? Rem quo, cupiditate quisquam numquam voluptate exercitationem iure iste repellendus quaerat tempora maxime ipsa accusamus ad. Voluptatum rem similique suscipit
-                                    .</p>
-                                    <a href="#" title='lire l\article en entier'>
-                                        <button type='button' className='btn btn-danger'>Continuer la lecture...</button>
-                                    </a>
-                                </div>
-            </article>) 
-    ; 
+const articles = [
+  { 
+    title: "Introduction à l'intelligence artificielle", 
+    content: "Découverte des bases de l'intelligence artificielle et de ses applications." 
+    },
+  { 
+    title: "Les technologies essentielles du développement web", 
+    content: "Les principaux outils pour créer des sites modernes et performants." 
+    },
+  { 
+    title: "Bonnes pratiques de cybersécurité en entreprise", 
+    content: "Les bonnes pratiques pour protéger ses données en ligne." 
+}
+];
+
+
+const listArticles = articles.map(a => {
+  return ( 
+    <article className='mt-4 card col-8 col-md-6 col-lg-3'>
+        <div className='card-body'>
+            <h3 className='bg-primary p-3 text-white card-title'>{a.title}</h3>
+            <p className='card-text text-body-secondary'>{a.content}</p>
+            <a href="#" title='lire l\article en entier'>
+            <button type='button' className='btn btn-danger'>Continuer la lecture...</button>
+            </a>
+        </div>
+    </article>
+  );
+})
+
+    return  displayArticle && (listArticles) 
+     ; 
 }
 
