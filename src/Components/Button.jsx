f85bf3cd8f56d85fbc0243e849c83720aae8edf5
Cassius269 <fahamygaston@gmail.com>
2026-01-10 21:22:17 +0100

    utilisation de plusieurs hooks useState() dans le composant enfant Button

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,15 +3,24 @@ import {useState} from 'react'; // Importer le hook useState()
 
 function Button({type, children}){
     const [count, setCount] = useState(0);
+    const [isCountShown, setCountShown] = useState(true);
 
-    const handleOnclick = (e) => {
+    const handleClickButtonSubmit = (e) => {
         console.log("bouton submit cliqué");
         setCount(count + 1); // incrémentation du compteur et mise à jour de la vue
         console.log(`Le compte de click (bouton submit) : ${count}`);
     };
 
-    console.log(`Nouvelle valeur du compteur: ${count}`)
-    return <button onClick={handleOnclick} type={type} className="btn btn-primary m-auto mt-5 col-11 col-md-6 col-lg-3">{children} <i>{count}</i></button>
+    const handleClickCount = () => {
+        setCountShown(!isCountShown);
+    }
+
+    return (
+        <>
+            <button onClick={handleClickButtonSubmit} type={type} className="btn btn-primary m-auto mt-5 col-11 col-md-6 col-lg-3">{children} {isCountShown && <i>{count}</i>}</button>
+            <button onClick={handleClickCount} className="btn btn-success m-auto mt-5 col-11 col-md-6 col-lg-3">{isCountShown ? "Cacher le compteur" : "Montrer le compteur"}</button>
+        </>
+    )
 }
 
 export default Button;
