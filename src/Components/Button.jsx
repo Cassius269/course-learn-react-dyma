e1299571c4accb75a8c7691dc9603b69898a56e5
Cassius269 <fahamygaston@gmail.com>
2026-01-10 20:09:33 +0100

    addition du compteur plutôt qu'incrémentation

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,16 +2,15 @@ import {useState} from 'react'; // Importer le hook useState()
 
 
 function Button({type, children}){
-    let [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-    console.log(useState(0))
     const handleOnclick = (e) => {
         console.log("bouton submit cliqué");
-        setCount(++count); // incrémentation du compteur et mise à jour de la vue
+        setCount(count + 1); // incrémentation du compteur et mise à jour de la vue
         console.log(`Le compte de click (bouton submit) : ${count}`);
     };
 
-    console.log(`Nouveau valeur du compteur: ${count}`)
+    console.log(`Nouvelle valeur du compteur: ${count}`)
     return <button onClick={handleOnclick} type={type} className="btn btn-primary m-auto mt-5 col-11 col-md-6 col-lg-3">{children} <i>{count}</i></button>
 }
 
