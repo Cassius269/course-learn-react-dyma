4f463b9e29d4f88af46a92f1a92a520cc873eec6
Cassius269 <fahamygaston@gmail.com>
2026-01-10 16:53:42 +0100

    correction du nom de la donnée pour le prénom

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -10,7 +10,7 @@ export default function Form(){
     }
 
     const handleFocus = (e) => {
-        console.log("Focus en cours", e.target);
+        console.log("Focus en cours sur l'input: ", e.target);
 
     }
 
@@ -19,7 +19,7 @@ export default function Form(){
             <h3 className="fs-2 mb-5">Formulaire d'inscription <i className="">info</i></h3>
             <form onSubmit={handleSubmit} action="" method="GET" className="row bg-primary-subtle p-5 rounded-2">
                 <InputPlusLabel onFocus={handleFocus} type="text" name="lastname" placeholder="Nom de Famille" />
-                <InputPlusLabel onFocus={handleFocus} type="text" name="text" placeholder="Prénom" />
+                <InputPlusLabel onFocus={handleFocus} type="text" name="firstname" placeholder="Prénom" />
                 <InputPlusLabel onFocus={handleFocus} type="email" name="email"  placeholder="email" />
                 <Button type="submit">Submit</Button>
                 {/* <button type="submit" className="btn btn-primary m-auto mt-5 col-11 col-md-6 col-lg-3">Soumettre</button>      */}
