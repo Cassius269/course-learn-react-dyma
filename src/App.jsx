664a8665aa3bde7e793cc5d5d9c744b7965e3cba
Cassius269 <fahamygaston@gmail.com>
2026-01-28 23:27:28 +0100

    transformer le composant titre en module par défaut

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Article from './components/article';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import { Title } from './components/Title';
+import Title from './components/Title';
 import Articles from './components/Articles';
 import Form from './components/Form';
 
--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,3 +1,5 @@
-export const Title = () => {
+const Title = () => {
     return <h1 className="bg-secondary py-5">Bienvenu dans le blog</h1>
 }
+
+export default Title;
